5a0195a991f0fc6bc99976782b970ce5e9187efe
twinitiee <twinitiee@gmail.com>
2018-07-24 14:49:40 +0000

    Done.

---
+++ holiday_greeting.rb
@@ -0,0 +1,5 @@
+def holiday_greeting(to, from, holiday)
+  "Happy #{holiday}, #{to}! From #{from}"
+end
+
+holiday_greeting("you", "me", "day")
--- mothers_day.rb
+++ mothers_day.rb
@@ -1,3 +1,9 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
+def mothers_day(name = "Mom")
+  "Happy Mother's Day, #{name}!"
+end
 
+mothers_day("Mom")
+
+mothers_day("Tracy")
